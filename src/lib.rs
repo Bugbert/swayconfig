ed0523dabf339317dedda68dc34449f1b818cd72
Bugbert <bugbert@duck.com>
2022-07-25 21:38:07 -0400

    got library to work

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub mod parser {
+    use std::fs;
+
+    pub fn stringify(file: String) -> String {
+        return fs::read_to_string(file)
+            .expect("Something went wrong reading the file");
     }
 }
