c8914459358324bd7c7e23813142c4ffa550523a
Bugbert <bugbert@duck.com>
2022-07-26 08:30:52 -0400

    added linereading capability

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,37 @@
 pub mod parser {
-    use std::fs;
+    use std::fs::File;
+    use std::io::{self, BufRead};
+    use std::path::Path;
 
-    pub fn stringify(file: String) -> String {
-        return fs::read_to_string(file)
-            .expect("Something went wrong reading the file");
+/*    struct Variable {
+        name: String,
+        value: String,
+    }
+
+    struct Keybind {
+        binding: String,
+        effect: String,
+    }
+    
+    struct ConfigFile {
+        wallpaper: String,
+        vars: Vec<Variable>,
+        keybinds: Vec<Keybind>,
+        on_start: Vec<String>,
+    }
+*/
+    pub fn file_to_struct(filename: String) {
+        if let Ok(lines) = read_lines(filename) {
+            for line in lines {
+                if let Ok(command) = line {
+                    println!("line: {}", command);
+                }
+            }
+        }
+    }
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
     }
 }
