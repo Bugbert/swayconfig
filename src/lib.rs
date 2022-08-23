7f3f9705fa704409b7283562fb7cb2d5eb086450
Bugbert <bugbert@duck.com>
2022-08-22 22:46:55 -0400

    Fixed A LOT. Need to document.

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,32 @@
+#![allow(dead_code)]
+
 pub mod parser {
     use std::fs;
 
-    pub enum CommandType {
-        StartupLine,
-        VariableLine,
-        KeybindLine,
-        IncludeLine,
-        CommentLine,
+    #[derive(Debug)]
+    pub enum Command {
+        StartupLine(u8, Vec<String>),
+        VariableLine(u8, Vec<String>),
+        KeybindLine(u8, Vec<String>),
+        IncludeLine(u8, Vec<String>),
+        CommentLine(u8, Vec<String>),
     }
 
-    #[derive(Clone, Debug)]
-    pub struct Variable {
-        name: String,
-        value: String,
-    }
-
-    #[derive(Clone, Debug)]
-    pub struct Keybind {
-        binding: String,
-        action: String,
-    }
-
-    #[derive(Clone, Debug)]
-    pub struct ConfigFile {
-        vars: Vec<Variable>,
-        keybinds: Vec<Keybind>,
-        startup: Vec<String>,
-        include: Vec<String>,
+    #[derive(Debug)]
+    pub enum Element {
+        Startup(String),
+        Variable(String, String),
+        Keybind(String, String),
+        Include(String),
+        Comment(String),
     }
 
     pub fn file_to_struct(filename: String) {
-        let mut config_file = ConfigFile {
-            vars: Vec::new(),
-            keybinds: Vec::new(),
-            startup: Vec::new(),
-            include: Vec::new(),
-        };
+        let mut config_file: Vec<Element> = Vec::new();
         let file_lines = read_lines(filename);
-        for command in file_lines {
-            let command_type: CommandType = get_type(&command);
-            config_file = match command_type {
-                CommandType::StartupLine => add_startup(config_file.clone(), command),
-                CommandType::VariableLine => add_var(config_file.clone(), command),
-                CommandType::KeybindLine => add_keybind(config_file.clone(), command),
-                CommandType::IncludeLine => add_include(config_file.clone(), command),
-                _ => panic!("I screwed up"),
-            };
+        for command_line in file_lines {
+            let command: Command = get_type(split_line(command_line));
+            config_file.push(command_to_element(command));
         }
         println!("{:?}", config_file);
     }
@@ -59,7 +40,7 @@ pub mod parser {
         let mut backslash: bool = false;
         let mut bracket: bool = false;
         let mut in_block: bool = false;
-        let mut line: Vec<char> = Vec::new();
+        let mut line: String = String::new();
         for c in contents_array {
             if c == '\\' {
                 backslash = true;
@@ -77,10 +58,8 @@ pub mod parser {
                 in_block = true;
                 bracket = false;
             } else if c == '\n' && line.len() != 0 && !in_block {
-                let line_clone = line.clone();
-                let line_string: String = line_clone.iter().collect();
-                file_lines.push(line_string);
-                line = Vec::new();
+                file_lines.push(line);
+                line = String::new();
             } else if c == '\n' && line.len() == 0 {
             } else if backslash == true {
                 line.push('\\');
@@ -92,133 +71,111 @@ pub mod parser {
         return file_lines;
     }
 
-    fn get_type(text: &String) -> CommandType {
-        let text_str: &str = &text[..];
-        let text_array = text_str.chars();
-        let mut first_word_vec: Vec<char> = Vec::new();
-        for c in text_array {
-            if c == ' ' && first_word_vec.len() != 0 {
-                break;
-            } else if c == ' ' && first_word_vec.len() == 0 {
-            } else if c == '#' && first_word_vec.len() == 0 {
-                return CommandType::CommentLine;
+    pub fn get_type(words: (u8, Vec<String>)) -> Command {
+        println!("{:?}", &words);
+        let first_word: &str = &words.1[0][..];
+        if first_word.chars().next().unwrap() == '#' {
+            return Command::CommentLine(words.0, words.1);
+        }
+        match first_word {
+            "set" => Command::VariableLine(words.0, words.1),
+            "exec" => Command::StartupLine(words.0, words.1),
+            "bindsym" => Command::KeybindLine(words.0, words.1),
+            "include" => Command::IncludeLine(words.0, words.1),
+            _ => panic!("faulty config file"),
+        }
+    }
+
+    pub fn split_line(line: String) -> (u8, Vec<String>) {
+        println!("{}", &line);
+        let line_str: &str = &line[..];
+        let line_array = line_str.chars();
+        let mut tabs: u8 = 0;
+        let mut current_word: Vec<char> = Vec::new();
+        let mut words: Vec<String> = Vec::new();
+        let mut in_dquotes: bool = false;
+        let mut in_squotes: bool = false;
+        for c in line_array {
+            if c == '"' && !in_dquotes && !in_squotes {
+                current_word.push(c);
+                in_dquotes = true;
+            } else if c == '\'' && !in_dquotes && !in_squotes {
+                current_word.push(c);
+                in_squotes = true;
+            } else if c == '"' && in_dquotes {
+                current_word.push(c);
+                let word: String = current_word.iter().collect();
+                words.push(word);
+                in_dquotes = false;
+                current_word = Vec::new();
+            } else if c == '\'' && in_squotes {
+                current_word.push(c);
+                let word: String = current_word.iter().collect();
+                words.push(word);
+                in_squotes = false;
+                current_word = Vec::new();
+            } else if c == ' ' && current_word.len() != 0 && !in_dquotes && !in_squotes {
+                let word: String = current_word.iter().collect();
+                words.push(word);
+                current_word = Vec::new();
+            } else if c == '\t' && current_word.len() == 0 && words.len() == 0 {
+                tabs += 1;
             } else {
-                first_word_vec.push(c);
+                current_word.push(c);
             }
         }
-        let first_word: String = first_word_vec.iter().collect();
-        let first_word_str: &str = &first_word[..];
-        let command_type: CommandType = match first_word_str {
-            "exec" => CommandType::StartupLine,
-            "set" => CommandType::VariableLine,
-            "bindsym" => CommandType::KeybindLine,
-            "include" => CommandType::IncludeLine,
-            _ => panic!("faulty config file \"{}\"", first_word_str.to_string()),
-        };
-        return command_type;
+        return (tabs, words);
     }
 
-    fn add_var(mut config_file: ConfigFile, command: String) -> ConfigFile {
-        let cmd_str: &str = &command[..];
-        let cmd_array = cmd_str.chars();
-        let mut name_vec: Vec<char> = Vec::new();
-        let mut value_vec: Vec<char> = Vec::new();
-        let mut start_name: bool = false;
-        let mut start_value: bool = false;
-        for c in cmd_array {
-            if c == '$' && !start_name && !start_value {
-                start_name = true;
-            } else if start_name && c == ' ' {
-                start_name = false;
-                start_value = true;
-            } else if start_name {
-                name_vec.push(c);
-            } else if start_value {
-                value_vec.push(c);
-            }
+    pub fn command_to_element(command: Command) -> Element {
+        match command {
+            Command::StartupLine(_, words) => make_startup(words),
+            Command::IncludeLine(_, words) => Element::Include(words.get(1).unwrap()[..]
+                .to_string()),
+            Command::KeybindLine(_, words) => make_keybind(words),
+            Command::VariableLine(_, words) => make_variable(words),
+            Command::CommentLine(_, words) => make_comment(words),
         }
-        let name: String = name_vec.iter().collect();
-        let value: String = value_vec.iter().collect();
-        let variable = Variable {
-            name: name,
-            value: value,
-        };
-        config_file.vars.push(variable);
-        return config_file;
     }
 
-    fn add_keybind(mut config_file: ConfigFile, command: String) -> ConfigFile {
-        let cmd_str: &str = &command[..];
-        let cmd_array = cmd_str.chars();
-        let mut bind_vec: Vec<char> = Vec::new();
-        let mut action_vec: Vec<char> = Vec::new();
-        let mut on_command: bool = false;
-        let mut start_bind: bool = false;
-        let mut start_action: bool = false;
-        for c in cmd_array {
-            if c != ' ' && !on_command && !start_bind && !start_action {
-                on_command = true;
-            } else if c == ' ' && on_command {
-                on_command = false;
-                start_bind = true;
-            } else if c != ' ' && start_bind {
-                bind_vec.push(c);
-            } else if c == ' ' && start_bind {
-                start_bind = false;
-                start_action = true;
-            } else if start_action {
-                action_vec.push(c);
-            }
+    pub fn make_startup(mut words: Vec<String>) -> Element {
+        words.remove(0); // removes the "exec"
+        let mut action: String = String::new();
+        for word in words {
+            action.push_str(&word[..]);
         }
-        let binding: String = bind_vec.iter().collect();
-        let action: String = action_vec.iter().collect();
-        let keybind = Keybind {
-            binding: binding,
-            action: action,
-        };
-        config_file.keybinds.push(keybind);
-        return config_file;
+        return Element::Startup(action);
     }
 
-    fn add_startup(mut config_file: ConfigFile, command: String) -> ConfigFile {
-        let cmd_str: &str = &command[..];
-        let cmd_array = cmd_str.chars();
-        let mut action_vec: Vec<char> = Vec::new();
-        let mut on_command: bool = false;
-        let mut start_action: bool = false;
-        for c in cmd_array {
-            if c != ' ' && !on_command && !start_action {
-                on_command = true;
-            } else if c == ' ' && on_command {
-                on_command = false;
-                start_action = true;
-            } else if start_action {
-                action_vec.push(c);
-            }
+    pub fn make_variable(mut words: Vec<String>) -> Element {
+        words.remove(0);
+        let var_name_ref = words.get(0).unwrap();
+        let var_name = var_name_ref[..].to_string();
+        words.remove(0);
+        let mut value = String::new();
+        for word in words {
+            value.push_str(&word[..]);
         }
-        let action: String = action_vec.iter().collect();
-        config_file.startup.push(action);
-        return config_file;
+        return Element::Variable(var_name, value);
     }
 
-    fn add_include(mut config_file: ConfigFile, command: String) -> ConfigFile {
-        let cmd_str: &str = &command[..];
-        let cmd_array = cmd_str.chars();
-        let mut inc_vec: Vec<char>= Vec::new();
-        let mut on_command: bool = false;
-        let mut start_inc: bool = false;
-        for c in cmd_array {
-            if c != ' ' && !on_command && !start_inc {
-                on_command = true;
-            } else if c == ' ' && on_command {
-                on_command = false;
-                start_inc = true;
-            } else if c != ' ' && start_inc {
-                inc_vec.push(c);
-            }
+    pub fn make_keybind(mut words: Vec<String>) -> Element {
+        words.remove(0);
+        let keybind_ref = words.get(0).unwrap();
+        let keybind = keybind_ref[..].to_string();
+        words.remove(0);
+        let mut action = String::new();
+        for word in &words {
+            action.push_str(&word[..]);
         }
-        let inc: String = inc_vec.iter().collect();
-        config_file.include.push(inc);
-        return config_file;
+        return Element::Keybind(keybind, action);
+    }
+
+    pub fn make_comment(words: Vec<String>) -> Element {
+        let mut comment = String::new();
+        for word in words {
+            comment.push_str(&word[..]);
+        }
+        return Element::Comment(comment);
     }
 }
